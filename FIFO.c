646180f85e894c3a1db8248da1976590fe980df7
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-10-04 11:56:40 +0530

    FIFO committed

--- FIFO.c
+++ FIFO.c
@@ -3,15 +3,16 @@
 int main()
 {
 	int n, page[50], frame[10],i,j,no;
+	int count=0;
 	printf("Enter the no. of pages:");
 	scanf("%d",&n);
-	printf("Enter the page number:");
+	printf("Enter the page number:\n");
 	for(int i=0;i<n;i++)
 		scanf("%d", &page[i]);
 	printf("Enter the  no. of frames:");
 	scanf("%d",&no);
 	for(int j=0;j<no;j++)
-		frame[i]=-1; //Denotes that the particular frame is empty
+		frame[j]=-1; //Denotes that the particular frame is empty 
 	printf("Ref String\t\tPage Frames\n");
 	printf("__________\t\t___________\n\n");
 	for(i=0;i<n;i++)
@@ -20,20 +21,27 @@ int main()
 		int flag=0;
 		for(j=0;j<no;j++)
 		{
-			if(frame[i]==item)
+			if(frame[j]==item)
 			{
 				flag=1;
 				break;
 			}
 		}
 		if(flag==0)
-		{
-			frame[(i%no)]=item;
+		{	
+			frame[(count%no)]=item;
+			count++;
 		}
 		printf("%d\t\t",item);
 		for(int j=0;j<no;j++)
-			printf("%d\t",frame[j]);
+		{
+			if(frame[j]==-1)
+				printf("X\t\t");
+			else		
+				printf("%d\t\t",frame[j]);
+		}
 		printf("\n");
+		
 	}
 	return 0;
 }
