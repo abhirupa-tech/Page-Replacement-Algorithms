0a7a316df18fc045f422610e6845abf4a0b24522
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-10-04 23:49:13 +0530

    All replacement algoritms added

--- LRU.c
+++ LRU.c
@@ -1,65 +1,80 @@
+
+	
 #include<stdio.h>
+ 
+int findLRU(int time[], int n){
+    int i, minimum = time[0], pos = 0;
+ 
+    for(i = 1; i < n; ++i){
+        if(time[i] < minimum){
+            minimum = time[i];
+            pos = i;
+        }
+    }
+    
+    return pos;
+}
+ 
 int main()
 {
-	int n,k,l, page[50], frame[10],i,j,no;
-	int frame_copy[10];
-	int count=0;
-	printf("Enter the no. of pages:");
-	scanf("%d",&n);
-	printf("Enter the page number:\n");
-	for(int i=0;i<n;i++)
-		scanf("%d", &page[i]);
-	printf("Enter the  no. of frames:");
-	scanf("%d",&no);
-	for(int j=0;j<no;j++)
-		frame[j]=-1; //Denotes that the particular frame is empty
-	printf("Ref String\t\tPage Frames\n");
-	printf("__________\t\t___________\n\n");
-	for(i=0;i<n;i++)
-	{
-		int item = page[i];
-		int flag=0;
-		for(j=0;j<no;j++)
-		{
-			if(frame[j]==item)
-			{
-				flag=1;
-				break;
-			}
-		}
-		if(flag==0)
-		{
-			for(k=0; k<no;k++)
-				frame_copy[k]=frame[k];
-			count=0;
-			for(k=i-1;k>=0;k--)
-			{
-				for(l=0;l<no;l++)
-				{
-					if(frame_copy[l]==page[k])
-					{
-						frame_copy[l]=-1;
-						count++;
-					}
-				}
-					
-				if (count==no-1)
-					break;
-			}
-
-		}
-		printf("%d\t\t",item);
-		for(int j=0;j<no;j++)
-		{
-			if(frame[j]==-1)
-				printf("X\t\t");
-			else
-				printf("%d\t\t",frame[j]);
-		}
-		printf("\n");
-
-	}
-	return 0;
+    int no_of_frames, no_of_pages, frames[10], pages[30], counter = 0, time[10], flag1, flag2, i, j, pos, faults = 0;
+    printf("Enter number of frames: ");
+    scanf("%d", &no_of_frames);
+    
+    printf("Enter number of pages: ");
+    scanf("%d", &no_of_pages);
+    
+    printf("Enter reference string: ");
+    
+    for(i = 0; i < no_of_pages; ++i){
+        scanf("%d", &pages[i]);
+    }
+    
+    for(i = 0; i < no_of_frames; ++i){
+        frames[i] = -1;
+    }
+    
+    for(i = 0; i < no_of_pages; ++i){
+        flag1 = flag2 = 0;
+        
+        for(j = 0; j < no_of_frames; ++j){
+            if(frames[j] == pages[i]){
+                counter++;
+                time[j] = counter;
+                   flag1 = flag2 = 1;
+                   break;
+               }
+        }
+        
+        if(flag1 == 0){
+            for(j = 0; j < no_of_frames; ++j){
+                if(frames[j] == -1){
+                    counter++;
+                    faults++;
+                    frames[j] = pages[i];
+                    time[j] = counter;
+                    flag2 = 1;
+                    break;
+                }
+            }    
+        }
+        
+        if(flag2 == 0){
+            pos = findLRU(time, no_of_frames);
+            counter++;
+            faults++;
+            frames[pos] = pages[i];
+            time[pos] = counter;
+        }
+        
+        printf("\n");
+        
+        for(j = 0; j < no_of_frames; ++j){
+            printf("%d\t", frames[j]);
+        }
+    }
+    
+    printf("\n\nTotal Page Faults = %d", faults);
+    
+    return 0;
 }
-
-
